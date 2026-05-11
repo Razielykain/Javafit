f49a59a4c94ca8b12aefc4fbb6663051733b3700
razie <razie@BOOK-MKKD7ASD2S>
2026-05-11 19:48:42 +0200

    Creacion clase Admin

---
+++ src/main/java/es/uah/javafit/model/usuarios/Admin.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.uah.javafit.model.usuarios;
+
+/**
+ *
+ * @author razie
+ */
+public class Admin extends User 
+{
+
+    public Admin(String user, String password) 
+    {
+        super(user, password);
+    }
+}
