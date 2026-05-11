8006e7b445bce5b129f80253b59e65fec09a1af5
razie <razie@BOOK-MKKD7ASD2S>
2026-05-11 17:01:20 +0200

    Initial commit :)

---
+++ src/main/java/es/uah/javafit/Javafit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.uah.javafit;
+
+/**
+ *
+ * @author razie
+ */
+public class Javafit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
