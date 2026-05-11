74f67b4181f6169186829ff975543a8e4cfc72f4
razie <razie@BOOK-MKKD7ASD2S>
2026-05-11 17:35:52 +0200

    Creacion DiaSemana (ENUM)

---
+++ src/main/java/es/uah/javafit/model/DiaSemana.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package es.uah.javafit.model;
+
+/**
+ *
+ * @author razie
+ */
+public enum DiaSemana 
+{
+    LUNES("Lunes"),
+    MARTES("Martes"),
+    MIERCOLES("Miercoles"),
+    JUEVES("Jueves"), 
+    VIERNES("Viernes"),
+    SABADO("Sabado"),
+    DOMINGO("Domingo");
+    
+    // atributo
+    private final String etiqueta;
+
+    private DiaSemana(String etiqueta) 
+    {
+        this.etiqueta = etiqueta;
+    }
+
+    public String getEtiqueta() 
+    {
+        return etiqueta;
+    }
+}
