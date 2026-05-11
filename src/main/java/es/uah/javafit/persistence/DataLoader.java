766a3dceacd5791a9021dbd5c9f89ab990627f04
razie <razie@BOOK-MKKD7ASD2S>
2026-05-11 20:10:33 +0200

    Creacion de clase DDataLoader para las pruebas

---
+++ src/main/java/es/uah/javafit/persistence/DataLoader.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.uah.javafit.persistence;
+//imports
+
+/**
+ *
+ * @author razie
+ */
+public class DataLoader 
+{
+    public static void cargarDatosPrueba(GestorGym g)
+    {
+        // rellenar luego
+    }
+}
