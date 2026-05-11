0ac50e1274644af2f9e1936f85c751b7cc4dd826
razie <razie@BOOK-MKKD7ASD2S>
2026-05-11 20:08:59 +0200

    Creacion de clase GestorPersistencia

---
+++ src/main/java/es/uah/javafit/persistence/GestorPersistencia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.uah.javafit.persistence;
+//imports
+/**
+ *
+ * @author razie
+ */
+public class GestorPersistencia 
+{
+    static final String RUTA_ARC = ""; //temp
+    
+    // no se si esto lleva constructor o no
+    
+    // no hay setters ni getters, es final
+    // Funciones y metodos
+    public static void guardar(GestorGym g)
+    {
+        // lo hare cuando aprenda a usar esto
+    }
+    
+    public static void cargar()
+    {
+        // en verdad devuelve GestorGym -> cambiar luego
+    }
+}
