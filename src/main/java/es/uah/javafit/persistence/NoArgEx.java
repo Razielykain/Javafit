59a992081b7ddbd34ece3e77f80f5c758eb4f5e0
Razielykain <>
2026-05-14 20:04:53 +0200

    NoArgEX exception class

---
+++ src/main/java/es/uah/javafit/persistence/NoArgEx.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.uah.javafit.persistence;
+
+/**
+ *
+ * @author razie
+ */
+public class NoArgEx extends Exception {
+
+    public NoArgEx(String msg) {
+        super(msg);
+    }
+    
+}
